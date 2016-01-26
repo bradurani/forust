81e8c6f6bfd1366d6c424194d59235efc08af7e2
Brad Urani <bradurani@gmail.com>
2016-01-26 01:12:45 -0800

    Refactor parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #[macro_use]
 extern crate clap;
-extern crate iterator_to_hash_map;
 
 use clap::App;
 use std::error::Error;
@@ -9,41 +8,64 @@ use std::io::prelude::*;
 use std::path::Path;
 use std::process::Command;
 use std::process::Stdio;
-use std::collections::HashMap;
-use iterator_to_hash_map::ToHashMap;
+use std::process::Child;
+use std::fmt;
+
+struct ShellCommand<'s> {
+    name: &'s str,
+    command: &'s str,
+    child: Child,
+}
+
+impl<'d> fmt::Debug for ShellCommand<'d> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,
+               "Command{{ name: {}, command: {} }}",
+               self.name,
+               self.command)
+    }
+}
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-
     let dir: &str = matches.value_of("directory").unwrap_or(".");
     let file: &str = matches.value_of("file").unwrap_or("Procfile");
+
     println!("{}/{}", dir, file);
     let contents: String = open_file(dir, file);
     println!("{}", contents);
-    let hm = parse_contents(&contents);
-    println!("{:?}", hm);
-}
-
-fn run_command(cmd: &str) {
-    let mut process = Command::new(cmd);
-    let mut child = process.spawn()
-                           .unwrap_or_else(|e| panic!("failed to execute child: {}", e));
-    let id = child.id();
+    let commands = parse_contents(&contents);
+    println!("{:?}", commands);
+    let shell_commands = run_commands(commands);
+    println!("{:?}", shell_commands);
     // let ecode = child.wait()
     //                  .unwrap_or_else(|e| panic!("failed to wait on child: {}", e));
     // println!("ecode {:?}", ecode);
-    id;
 }
 
-fn parse_contents<'m>(contents: &'m str) -> HashMap<&'m str, &'m str> {
-    let lines = contents.lines()
-                        .collect::<Vec<&str>>();
-    let iter = lines.iter();
-    let collection = iter.map(|s| s.split(':').collect::<Vec<&str>>())
-                         .collect::<Vec<Vec<&str>>>();
-    collection.to_hash_map(|i: &Vec<&'m str>| -> &'m str { i[0] },
-                           |i: &Vec<&'m str>| -> &'m str { i[1] })
+fn run_commands<'f>(commands: Vec<(&'f str, &'f str)>) -> Vec<ShellCommand> {
+    commands.into_iter()
+            .map(|(name, command)| run_command(name, command))
+            .collect::<Vec<_>>()
+}
+
+fn run_command<'f>(name: &'f str, command: &'f str) -> ShellCommand<'f> {
+    let mut process = Command::new(command);
+    let mut child = process.spawn()
+                           .unwrap_or_else(|e| panic!("failed to execute child: {}", e));
+    ShellCommand {
+        name: name,
+        command: command,
+        child: child,
+    }
+}
+
+fn parse_contents<'p>(contents: &'p String) -> Vec<(&'p str, &'p str)> {
+    contents.lines()
+            .map(|s| s.split(':').collect::<Vec<&str>>())
+            .map(|a| (a[0], a[1]))
+            .collect::<Vec<(&str, &str)>>()
 }
 
 fn open_file(dir: &str, file: &str) -> String {
